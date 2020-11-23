67b4caba7051d5651d33ce1a59b296b3ddaa0fb9
Dremora Restless <drestles@vo-j5.msk.21-school.ru>
2020-11-23 16:58:11 +0300

    delete 2 button

--- day00/day00/CornerButton.swift
+++ day00/day00/CornerButton.swift
@@ -9,14 +9,6 @@
 import UIKit
 
 class CornerButton: UIButton {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
     
     override func layoutSubviews() {
         super.layoutSubviews()
--- day00/day00/ViewController.swift
+++ day00/day00/ViewController.swift
@@ -9,39 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var widthConstraintNulButton: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
-    override func viewDidLayoutSubviews() {
-        // calculate nul button width
-        let miniWidth: CGFloat = ( UIScreen.main.bounds.width - (2 * 12 + 3 * 8) ) / 4.0
-        let bigWidth: CGFloat = miniWidth * 2 + 8
-        widthConstraintNulButton = widthConstraintNulButton.changeMultiplier(multiplier: bigWidth / miniWidth)
-    }
-
-}
-
-extension NSLayoutConstraint {
-    func changeMultiplier(multiplier: CGFloat) -> NSLayoutConstraint {
-        let newConstraint = NSLayoutConstraint(
-            item: firstItem,
-            attribute: firstAttribute,
-            relatedBy: relation,
-            toItem: secondItem,
-            attribute: secondAttribute,
-            multiplier: multiplier,
-            constant: constant)
-        newConstraint.priority = priority
-        
-        NSLayoutConstraint.deactivate([self])
-        NSLayoutConstraint.activate([newConstraint])
-        
-        return newConstraint
-    }
     
 }
+
