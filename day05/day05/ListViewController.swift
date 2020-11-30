7b1195d5eae196c118faebac27def44906d6b075
Dremora Restless <drestles@ox-c3.msk.21-school.ru>
2020-11-30 23:35:53 +0300

    model

--- day05/day05/ListViewController.swift
+++ day05/day05/ListViewController.swift
@@ -9,15 +9,46 @@ import UIKit
 
 class ListViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var places = [Place]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         if let path = Bundle.main.url(forResource: "data", withExtension: "json") {
             if let data = try? Data(contentsOf: path) {
-                print(data)
+                if let dic = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [Any] {
+                    for place in dic {
+                        if let placeDic = place as? [String: Any] {
+                            print(placeDic)
+                            let placeModel = Place(name: "", desc: "")
+                            print("====")
+                        }
+                    }
+//                    for place in dic {
+//                        if let name = place["name"] as? String,
+//                            let desc = place["desc"] as? String {
+//                            
+//                        }
+//                    }
+                }
 //                allWords = startWords.components(separatedBy: "\n")
             }
         }
     }
-    
 
+}
+
+extension ListViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return places.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //
+        return UITableViewCell()
+    }
+    
+    
 }
---
+++ day05/day05/Place.swift
@@ -0,0 +1,13 @@
+//
+//  Place.swift
+//  day05
+//
+//  Created by Dremora Restless on 11/30/20.
+//
+
+import Foundation
+
+struct Place {
+    let name: String
+    let desc: String
+}
