52f61b05eb8a1c240fb6ff070623605f7fdbb256
Kseniya Lukoshkina <xvluk@ya.ru>
2020-12-02 02:38:51 +0300

    ex01: gravity

--- day06/day06/ViewController.swift
+++ day06/day06/ViewController.swift
@@ -9,6 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var animator: UIDynamicAnimator!
+    var gravity: UIGravityBehavior!
+    var collision: UICollisionBehavior!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        animator = UIDynamicAnimator(referenceView: view)
+        collision = UICollisionBehavior()
+        
+        let recognizer = UITapGestureRecognizer()
+        recognizer.addTarget(self, action: #selector(handleLongPressGesture(_:)))
+        view.addGestureRecognizer(recognizer)
+        
+        
+    }
+    
     @objc func handleLongPressGesture(_ gestureRecognizer: UITapGestureRecognizer) {
         let location = gestureRecognizer.location(in: self.view)
         addShape(location: location)
@@ -16,19 +32,27 @@ class ViewController: UIViewController {
         
         print("gesture")
     }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let recognizer = UITapGestureRecognizer()
-        recognizer.addTarget(self, action: #selector(handleLongPressGesture(_:)))
-        view.addGestureRecognizer(recognizer)
-        
-    }
     
     private func addShape(location: CGPoint) {
 //        guard let x = location.x as? Int, let y = location.y as? Int else { return }
-        view.addSubview(ShapeView(x: location.x, y: location.y))
+//        view.addSubview(ShapeView(x: location.x, y: location.y))
+//        DispatchQueue.main.async {
+//            var animator = UIDynamicAnimator(referenceView: self.view)
+//            let shape = ShapeView(x: location.x, y: location.y)
+//            self.view.addSubview(shape)
+//            var gravity = UIGravityBehavior(items: [shape])
+//            gravity.magnitude = .greatestFiniteMagnitude
+//            animator.addBehavior(gravity)
+//        }
+        let square = ShapeView(x: location.x, y: location.y)
+        view.addSubview(square)
+        
+        gravity = UIGravityBehavior(items: [square])
+        animator.addBehavior(gravity)
+        
+        collision.addItem(square)
+        collision.translatesReferenceBoundsIntoBoundary = true
+        animator.addBehavior(collision)
     }
 
 
