0cddc01f22f091cddebb806e1a1019cb86e042e9
Kseniya Lukoshkina <xvluk@ya.ru>
2020-12-02 04:49:31 +0300

    pan gesture

--- day06/day06/ShapeView.swift
+++ day06/day06/ShapeView.swift
@@ -16,10 +16,29 @@ enum Shape {
 
 var allColors: [UIColor] = [.red, .black, .green, .blue]
 
+
 class ShapeView: UIView {
+
+//    override var collisionBoundingPath: UIBezierPath { return circularPath() }
 
     var shape: Shape?
     var color: UIColor?
+    
+    override var collisionBoundsType: UIDynamicItemCollisionBoundsType {
+        guard let shape = self.shape else { return .rectangle }
+        return shape == .circle ? .ellipse : .rectangle
+    }
+    
+//    @objc func handlePanGesture(_ gestureRecognizer: UIPanGestureRecognizer) {
+//        print("GR - pan")
+//        let location = gestureRecognizer.location(in: self)
+////        self.frame.origin.x = location.x
+////        self.frame.origin.y = location.y
+//        self.frame = CGRect(x: location.x, y: location.y, width: self.frame.size.width, height: self.frame.size.height)
+//        print(gestureRecognizer)
+//        print(location)
+////        addShape(location: location)
+//    }
     
     init(x: CGFloat, y: CGFloat) {
         let shape = Shape.allShapes[Int.random(in: 0..<Shape.allShapes.count)]
@@ -28,20 +47,21 @@ class ShapeView: UIView {
         self.color = color
         
         super.init(frame: CGRect(x: x, y: y, width: 100, height: 100))
+        
+//        let panPercognizer = UIPanGestureRecognizer()
+//        panPercognizer.addTarget(self, action: #selector(handlePanGesture(_:)))
+//        self.addGestureRecognizer(panPercognizer)
+        
         if shape == .circle {
             self.layer.masksToBounds = true
             self.layer.cornerRadius = 50
         }
+        
         self.backgroundColor = color
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
-    
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    
 
 }
--- day06/day06/ViewController.swift
+++ day06/day06/ViewController.swift
@@ -12,47 +12,73 @@ class ViewController: UIViewController {
     var animator: UIDynamicAnimator!
     var gravity: UIGravityBehavior!
     var collision: UICollisionBehavior!
+    var dynamicItem: UIDynamicItemBehavior!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         animator = UIDynamicAnimator(referenceView: view)
+        gravity = UIGravityBehavior()
         collision = UICollisionBehavior()
+        collision.translatesReferenceBoundsIntoBoundary = true
+        dynamicItem = UIDynamicItemBehavior()
+        dynamicItem.elasticity = 0.5
         
-        let recognizer = UITapGestureRecognizer()
-        recognizer.addTarget(self, action: #selector(handleLongPressGesture(_:)))
-        view.addGestureRecognizer(recognizer)
+        animator.addBehavior(gravity)
+        animator.addBehavior(collision)
+        animator.addBehavior(dynamicItem)
         
-        
+        let tapRecognizer = UITapGestureRecognizer()
+        tapRecognizer.addTarget(self, action: #selector(handleTapGesture(_:)))
+        view.addGestureRecognizer(tapRecognizer)
     }
     
-    @objc func handleLongPressGesture(_ gestureRecognizer: UITapGestureRecognizer) {
+    @objc func handleTapGesture(_ gestureRecognizer: UITapGestureRecognizer) {
+        print("GR - tap")
         let location = gestureRecognizer.location(in: self.view)
         addShape(location: location)
-//        self.view.addSubview(ShapeView(x: location.x, y: location.y))
-        
-        print("gesture")
     }
     
+    @objc func handlePanGesture(_ gestureRecognizer: UIPanGestureRecognizer) {
+        print("GR - pan")
+//        gestureRecognizer.view?.frame = CGRect(x: gestureRecognizer.location(in: self.view).x, y: gestureRecognizer.location(in: self.view).y, width: gestureRecognizer.view?.frame.size.width ?? 0, height: gestureRecognizer.view?.frame.size.height ?? 0)
+        guard let shapeView = gestureRecognizer.view as? ShapeView else { return }
+        switch gestureRecognizer.state {
+            case .began:
+                gravity.removeItem(shapeView)
+            case .changed:
+                collision.removeItem(shapeView)
+                dynamicItem.removeItem(shapeView)
+//                shapeView.center = gestureRecognizer.location(in: self.view)
+                shapeView.center.x += gestureRecognizer.translation(in: self.view).x
+                shapeView.center.y += gestureRecognizer.translation(in: self.view).y
+                collision.addItem(shapeView)
+                dynamicItem.addItem(shapeView)
+                animator.updateItem(usingCurrentState: shapeView)
+                gestureRecognizer.setTranslation(.zero, in: view)
+            case .ended, .cancelled, .failed:
+                gravity.addItem(shapeView)
+            default:
+                break
+        }
+    }
+//    @objc func handlePanGesture(_ gestureRecognizer: UITapGestureRecognizer) {
+//        print("GR - pan")
+//        let location = gestureRecognizer.location(in: self.view)
+//        addShape(location: location)
+//    }
+    
     private func addShape(location: CGPoint) {
-//        guard let x = location.x as? Int, let y = location.y as? Int else { return }
-//        view.addSubview(ShapeView(x: location.x, y: location.y))
-//        DispatchQueue.main.async {
-//            var animator = UIDynamicAnimator(referenceView: self.view)
-//            let shape = ShapeView(x: location.x, y: location.y)
-//            self.view.addSubview(shape)
-//            var gravity = UIGravityBehavior(items: [shape])
-//            gravity.magnitude = .greatestFiniteMagnitude
-//            animator.addBehavior(gravity)
-//        }
+
         let square = ShapeView(x: location.x, y: location.y)
         view.addSubview(square)
-        
-        gravity = UIGravityBehavior(items: [square])
-        animator.addBehavior(gravity)
-        
+    
+        gravity.addItem(square)
         collision.addItem(square)
-        collision.translatesReferenceBoundsIntoBoundary = true
-        animator.addBehavior(collision)
+        dynamicItem.addItem(square)
+        
+        let thePanRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture(_:)))
+        square.addGestureRecognizer(thePanRecognizer)
     }
 
 
