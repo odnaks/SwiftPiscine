222923f4f3770e69c988aec2b9a6808110067236
Dremora Restless <drestles@vo-k7.msk.21-school.ru>
2020-12-02 21:17:58 +0300

    init

---
+++ day07/day07/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  day07
+//
+//  Created by Dremora Restless on 12/2/20.
+//  Copyright © 2020 Dremora Restless. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
+
+//https://cocoapods.org/pods/RecastAI
+//https://cai.tools.sap/xvluk/day07/settings/tokens
+
+//https://cocoapods.org/pods/SwiftOpenWeatherMapAPI
+//https://home.openweathermap.org/api_keys
