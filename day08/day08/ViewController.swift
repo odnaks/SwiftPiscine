570e6b504906174faef1a75905acf17c0a3ea037
Dremora Restless <drestles@vo-k7.msk.21-school.ru>
2020-12-02 22:10:36 +0300

    research

---
+++ day08/day08/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  day08
+//
+//  Created by Dremora Restless on 12/2/20.
+//  Copyright © 2020 Dremora Restless. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
